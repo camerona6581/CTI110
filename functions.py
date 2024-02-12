36c17c2f0b754f562d690c4eb89725ede27cfa31
camerona6581 <78610579+camerona6581@users.noreply.github.com>
2024-02-12 14:51:06 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,19 @@
+#get number of hours worked
+def getHoursWorked():
+    hours = float(input("Enter the employee's hours worked: "))
+    return hours
+
+#get employee pay rate
+
+def getPayRate():
+    rate = float(input("Enter the employee's pay rate: "))
+    return rate
+# calculate the gross pay
+def calculateGrossPay(hoursWorked, payRate):
+    return hoursWorked * payRate
+    #return grossPay
+
+def displayValues(rate, hours, gross):
+    print(rate)
+    print(hours)
+    print(gross)
